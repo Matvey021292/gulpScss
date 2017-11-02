0d4f8fff99c98680b4ac5ae0565ad1c7b73980e9
Matvey <raul021292@gmail.com>
2017-11-02 11:12:21 +0200

    scss

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+ * Third party
+ */
+
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/js/bootstrap.min.js
+
+
+/*
+ * Custom
+ */
+//= partials/custom.js
